f781ba20841a822765c8644eb8c59f2ecaaca76e
albondean <103225216+albondean@users.noreply.github.com>
2022-08-24 12:24:16 +1200

    Add files via upload

--- python.py
+++ python.py
@@ -1,8 +1,27 @@
 QUESTIONS =["Is North America a Continent ?",
             "Do Genetic Females have XY chromosomes ?",
             "Do cheetahs bark ?",
-            "Is Eminem 5`11 ?"]
-print('Enter "true" (t) or "false" (f) for the following question, Enjoy!')
+            "Is Eminem 5`11 ?",
+            "Was Fortnite released in 2019?",
+            "Did Hitler die in 1953?",
+             "Do babies have more bones that adults?",
+             "Are there 7.97 Billion people in the world?",
+             "Did the lego company start off by makiing wooden toys?",
+             "Did I ask?"]
+ANSWER =["incorrect",
+         "correct, North America is a continent but the USA is a country",
+         "correct, Gentic Females actually have XX chromosomes",
+         "correct, Yes! Cheetahs are more related to dogs than cats due to the non-retractable claws and the barking gene",
+         "correct, He was actually 5`8",
+         "correct, Fortnite was realeased in 2017",
+         "correct, Hitler died in 1945",
+         "correct, babies have 305 bones whereas adults have 206",
+         "correct, there are 7,978,458,775 poeple in the world",
+         "correct, the creator of lego made wooden toys at first in 1932",
+         "correct, I did not ask and neither did anyone else",
+         "incorrect, I actually didn't ask"]
+
+print('Enter "true" or "false" for the following question, Enjoy!')
 valid_answer = False
 while not valid_answer:
         answer = input(QUESTIONS[0])
@@ -13,9 +32,9 @@ while not valid_answer:
 
         if valid_answer:
             if answer == 'true':
-                print('correct, North America is a continent but the USA is a country')
+                print(ANSWER[1])
             elif answer == 'false' :
-                print('incorrect ')
+                print(ANSWER[0])
 valid_answer = False
 while not valid_answer:
         answer = input(QUESTIONS[1])
@@ -26,9 +45,9 @@ while not valid_answer:
 
         if valid_answer:
             if answer == 'false':
-                print('correct, Gentic Females actually have XX chromosomes')
+                print(ANSWER[2])
             elif answer == 'true' :
-                print('incorrect ')
+                print(ANSWER[0])
 valid_answer = False
 while not valid_answer:
         answer = input(QUESTIONS[2])
@@ -39,9 +58,9 @@ while not valid_answer:
 
         if valid_answer:
             if answer == 'true':
-                print('correct, Yes! Cheetahs are more related to dogs than cats due to the non-retractable claws and the barking gene')
+                print(ANSWER[3])
             elif answer == 'false' :
-                print('incorrect ')
+                print(ANSWER[0])
 valid_answer = False
 while not valid_answer:
         answer = input(QUESTIONS[3])
@@ -52,7 +71,99 @@ while not valid_answer:
 
         if valid_answer:
             if answer == 'false':
-                print('correct, He was actually 5`8 ')
+                print(ANSWER[4])
             elif answer == 'true' :
-                print('incorrect ')
+                print(ANSWER[0])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[4])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
 
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[5])
+            elif answer == 'true' :
+                print(ANSWER[0])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[5])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[6])
+            elif answer == 'true' :
+                print(ANSWER[0])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[6])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[7])
+            elif answer == 'true' :
+                print(ANSWER[0])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[7])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[0])
+            elif answer == 'true' :
+                print(ANSWER[8])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[8])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[0])
+            elif answer == 'true' :
+                print(ANSWER[9])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[9])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[0])
+            elif answer == 'true' :
+                print(ANSWER[10])
+valid_answer = False
+while not valid_answer:
+        answer = input(QUESTIONS[10])
+        valid_answer = True
+        if answer != 'true' and answer != 'false':
+            print('try again')
+            valid_answer = False
+
+        if valid_answer:
+            if answer == 'false':
+                print(ANSWER[11])
+            elif answer == 'true' :
+                print(ANSWER[0])
+
+print("goodbye")
